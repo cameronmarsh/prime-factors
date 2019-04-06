f91f4a86515c10d97c816dae157f0e05b8b9e7bc
Cameron Marsh <cmarsh10@gmail.com>
2019-04-06 14:45:42 -0400

    refactored factorization algorithm again

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactors {
@@ -7,13 +8,13 @@ public class PrimeFactors {
         List<Integer> factors = new ArrayList<>();
 
         if (num == 1) {
-            return Arrays.asList(1);
-        } else {
-            for(int divisor = 2; num > 1; divisor++){
-                while (num % divisor == 0) {
-                    factors.add(divisor);
-                    num /= divisor;
-                }
+            return Collections.singletonList(1);
+        }
+
+        for (int divisor = 2; num > 1; divisor++) {
+            while (num % divisor == 0) {
+                factors.add(divisor);
+                num /= divisor;
             }
         }
 
